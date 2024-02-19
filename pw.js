dd6741e5d9ec4f404e70a5346f6c9a78b5770b72
Alex <103459735+torn80beta@users.noreply.github.com>
2024-02-19 12:59:59 +0200

    use webkit browser

--- pw.js
+++ pw.js
@@ -1,7 +1,7 @@
 const playwright = require("playwright");
 
 const parser = async ({ url }) => {
-  const browser = await playwright.firefox.launch({
+  const browser = await playwright.webkit.launch({
     headless: true,
   });
 
